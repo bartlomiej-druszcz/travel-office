50068b09b5da3c4cd87b3f3c0c435188b91461d8
bartlomiej-druszcz <bartlomiej.druszcz@gmail.com>
2023-04-22 13:32:17 +0200

    init commit

---
+++ src/main/java/com/example/traveloffice/TravelOfficeApplication.java
@@ -0,0 +1,13 @@
+package com.example.traveloffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelOfficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelOfficeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/traveloffice/TravelOfficeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.traveloffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelOfficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
